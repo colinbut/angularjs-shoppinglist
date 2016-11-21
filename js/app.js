9cba4d2c89d31c629277470073252c83eadb7d81
Colin But <colinbut@users.noreply.github.com>
2016-11-21 21:11:03 +0000

    tidied up routing plus error handling

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ var app = angular.module("shoppingList", ['ngRoute', 'shoppingListControllers'])
 app.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
-    when("/", {
+    when("/view", {
       templateUrl: 'partials/shoppingList.html',
       controller: 'shoppingListController'
+    })
+    .otherwise({
+      template: "<h1>Please navigate to correct path to view shopping list</h1>"
     });
 
     $locationProvider.html5Mode(false).hashPrefix('!');
--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,30 @@
 var shoppingListControllers = angular.module('shoppingListControllers',[]);
 
 shoppingListControllers.controller("shoppingListController", function($scope) {
+
+  // just initialise the list of products with some data on page load
   $scope.products = ["Milk", "Bread", "Cheese"];
 
-  $scope.addItem = function () {
-    $scope.products.push($scope.newProductItem);
+  $scope.addItem = function() {
+    $scope.errorText = "";
+
+    if (!$scope.newProductItem) {
+      return;
+    }
+
+    // prevent duplicate items added:
+    // if the new product item to be added is not already on the list
+    if ($scope.products.indexOf($scope.newProductItem) == -1) {
+      $scope.products.push($scope.newProductItem);
+    } else {
+      $scope.errorText = "The item is already in your shopping list.";
+    }
+
+
   }
 
   $scope.removeItem = function(x) {
+    $scope.errorText = "";
     $scope.products.splice(x,1);
   }
 
